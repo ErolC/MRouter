9a26bd952fcbb95fe30502fe1a8f7e8ddcecc04a
erolc <1287827600@qq.com>
2024-02-06 17:51:08 +0800

    fix

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -4,27 +4,22 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import com.erolc.mrouter.RouteHost
+import com.erolc.mrouter.backstack.LocalWindowScope
 import com.erolc.mrouter.register.page
 import com.erolc.mrouter.route.Exit
 import com.erolc.mrouter.scope.LocalPageScope
@@ -33,6 +28,7 @@ import com.erolc.mrouter.scope.rememberLazyListState
 import com.erolc.mrouter.utils.log
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
+import kotlin.math.roundToInt
 
 @Composable
 fun App() {
@@ -165,6 +161,45 @@ fun Home() {
                     }
                 }
             }, fontSize = 20.sp)
+        }
+    }
+}
+
+
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+internal fun PageTemplate(onBack: (() -> Unit)?, content: @Composable () -> Unit) {
+    val squareSize = LocalWindowScope.current.windowSize.value.width.size
+    val swipeableState = rememberSwipeableState(0) {
+        onBack != null
+    }
+    DisposableEffect(swipeableState.currentValue) {
+        if (swipeableState.currentValue == 1) {
+            onBack?.invoke()
+        }
+        onDispose {}
+    }
+    val sizePx = with(LocalDensity.current) { squareSize.toPx() }
+    val anchors = mapOf(0f to 0, sizePx to 1) // Maps anchor points (in px) to states
+    Box {
+        Box(
+            modifier = Modifier.fillMaxHeight().width(10.dp).swipeable(
+                state = swipeableState,
+                anchors = anchors,
+                thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                orientation = Orientation.Horizontal
+            )
+        )
+        Box(modifier = Modifier.offset {
+            if (onBack != null)
+                IntOffset(swipeableState.offset.value.roundToInt(), 0)
+            else
+                IntOffset.Zero
+
+        }) {
+            content()
         }
     }
 }
