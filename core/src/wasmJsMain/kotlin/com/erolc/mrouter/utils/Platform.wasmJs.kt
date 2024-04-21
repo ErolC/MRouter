58f31558043fd395dc25f4c0d7c66d86976782c6
erolc <1287827600@qq.com>
2024-04-21 12:05:26 +0800

    初步解决报错问题

--- core/src/wasmJsMain/kotlin/com/erolc/mrouter/utils/Platform.wasmJs.kt
+++ core/src/wasmJsMain/kotlin/com/erolc/mrouter/utils/Platform.wasmJs.kt
@@ -1,17 +1,31 @@
 package com.erolc.mrouter.utils
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.erolc.mrouter.backstack.entry.LocalWindowScope
 import com.erolc.mrouter.backstack.entry.WindowEntry
 import com.erolc.mrouter.dialog.DialogOptions
 import com.erolc.mrouter.model.WindowOptions
 import com.erolc.mrouter.window.WindowSize
+import kotlinx.browser.document
+import kotlinx.browser.window
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.w3c.dom.events.Event
 
 @Composable
 actual fun PlatformWindow(
@@ -19,9 +33,13 @@ actual fun PlatformWindow(
     entry: WindowEntry,
     content: @Composable () -> Unit
 ) {
+    val dpSize = DpSize(window.outerWidth.dp, window.outerHeight.dp)
+    val size by remember(dpSize) {
+        mutableStateOf(WindowSize.calculateFromSize(dpSize))
+    }
+    LocalWindowScope.current.windowSize.value = size
     content()
 }
-
 
 @Composable
 actual fun PlatformDialog(
