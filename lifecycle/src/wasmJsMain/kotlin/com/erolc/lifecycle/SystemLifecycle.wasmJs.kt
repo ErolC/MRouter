dacbb74a8c2e60359438c7b593c7d50027c06ede
erolc <1287827600@qq.com>
2024-04-24 23:17:33 +0800

    完善web生命周期

--- lifecycle/src/wasmJsMain/kotlin/com/erolc/lifecycle/SystemLifecycle.wasmJs.kt
+++ lifecycle/src/wasmJsMain/kotlin/com/erolc/lifecycle/SystemLifecycle.wasmJs.kt
@@ -2,23 +2,17 @@ package com.erolc.lifecycle
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
 import kotlinx.browser.window
 import org.w3c.dom.events.Event
-import org.w3c.dom.events.EventListener
 
 @Composable
 actual fun SystemLifecycle(call: (Lifecycle.Event) -> Unit) {
     DisposableEffect(window) {
-        val blurListener = object : EventListener {
-            override fun handleEvent(event: Event) {
-                call(Lifecycle.Event.ON_PAUSE)
-            }
+        val blurListener = { _: Event ->
+            call(Lifecycle.Event.ON_PAUSE)
         }
-        val focusListener = object : EventListener {
-            override fun handleEvent(event: Event) {
-                call(Lifecycle.Event.ON_RESUME)
-            }
+        val focusListener = { _: Event ->
+            call(Lifecycle.Event.ON_RESUME)
         }
         window.addEventListener("blur", blurListener)
         window.addEventListener("focus", focusListener)
