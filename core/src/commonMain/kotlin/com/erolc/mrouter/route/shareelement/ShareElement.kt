7429197e3ec35e4edc880220c7d297d96c5d6f6a
erolc <1287827600@qq.com>
2024-05-22 00:25:27 +0800

    共享元素去掉无意义的数据

--- core/src/commonMain/kotlin/com/erolc/mrouter/route/shareelement/ShareElement.kt
+++ core/src/commonMain/kotlin/com/erolc/mrouter/route/shareelement/ShareElement.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.core.Transition
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -24,8 +23,7 @@ import com.erolc.mrouter.utils.Init
 import kotlinx.coroutines.flow.MutableStateFlow
 
 /**
- * 共享元素,需要注意的是，共享元素是需要有大小变换的，那么就需要[content]中的界面调整为[fillMaxSize]，而该元素的具体大小请使用[modifier]进行设置,
- * 也就是共享元素的大小将由[Element]决定，而其内部的[content]只需要占满[Element]即可
+ * 共享元素
  * @param key 共享元素的标识
  * @param modifier 共享元素的修饰符
  * @param content 共享元素的界面
@@ -47,7 +45,8 @@ fun Element(key: String, modifier: Modifier, content: @Composable Transition<Sha
     }
     val state by element.state
     Box(modifier = modifier.background(Color.Transparent).onGloballyPositioned {
-        position.value = it.boundsInRoot()
+        val bound = it.boundsInRoot()
+        if (!bound.isEmpty) position.value = bound
     }) {
         if (state == Init || state == BeforeStart || state == BeforeEnd) content(updateTransition(state))
     }
